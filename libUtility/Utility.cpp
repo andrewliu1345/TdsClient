6a7c3c90176906c0edc0350761c2990d5d09aa7a
andrewliu1345 <andrewldq@qq.com>
2018-06-05 14:46:09 +0800

    完成数据解包与身份证获取信息

--- libUtility/Utility.cpp
+++ libUtility/Utility.cpp
@@ -1,10 +1,7 @@
 #include "stdafx.h"
 #include "Utility.h"
 
-#include <iostream>
 
-
-typedef list<sParam> PARAMLIST;
 
 Utility::Utility()
 {
@@ -37,7 +34,7 @@ unsigned char * Utility::toPackData(unsigned char *cmd, unsigned char fun, int c
 
 		intToByteArray(p.ParamLen, &tmp[index]);//n 参数长度
 		index += 2;
-		memcpy(&tmp[index], p.ParamData,  p.ParamLen);// n 参数
+		memcpy(&tmp[index], p.ParamData, p.ParamLen);// n 参数
 		datalen += (p.ParamLen + 2);
 		index += p.ParamLen;
 	}
@@ -52,11 +49,48 @@ unsigned char * Utility::toPackData(unsigned char *cmd, unsigned char fun, int c
 	index++;
 	intToByteArray(datalen, &sendBuffer[index]);//数据长度
 	index += 2;
-	memcpy(&sendBuffer[index], tmp,  datalen);
+	memcpy(&sendBuffer[index], tmp, datalen);
 	index += datalen;
 	sendBuffer[index++] = cr_bcc(tmp, datalen);
 	sendBuffer[index] = 0x03;
 	return sendBuffer;
+}
+
+int Utility::unPackData(unsigned char * buffer, int mun, PARAMLIST *params)
+{
+	int len = 0;
+	unsigned char blen[2] = { 0 };
+	memcpy(blen, &buffer[1], 2);
+	len = ByteArrayToInt(blen);
+
+	int tag1 = buffer[6];
+	int tag2 = buffer[7];
+	if (tag1 != 0 || tag2 != 0)
+	{
+		return -1;
+	}
+	int index = 8;
+	if (len > 5)
+	{
+		for (size_t i = 0; i < mun; i++)
+		{
+			sParam _param;
+			int lenx = 0;
+			unsigned char blenx[2] = { 0 };
+			memcpy(blenx, &buffer[index], 2);
+			lenx = ByteArrayToInt(blenx);
+			index += 2;
+
+			_param.ParamData = new unsigned char[lenx];
+			memset(_param.ParamData, 0, lenx);
+			memcpy(_param.ParamData, &buffer[index], lenx);
+			index += lenx;
+			_param.ParamLen = lenx;
+			params->push_back(_param);
+		}
+
+	}
+	return 0;
 }
 
 void Utility::intToByteArray(int a, unsigned char *b)
@@ -66,6 +100,13 @@ void Utility::intToByteArray(int a, unsigned char *b)
 		(unsigned char)(a & 0xFF)
 	};
 	memcpy(b, uc, 2);
+}
+
+int Utility::ByteArrayToInt(unsigned char * b)
+{
+	int a;
+	a = ((b[0] << 8) & 0xFF) + (b[1] & 0xFF);
+	return a;
 }
 
 unsigned char Utility::cr_bcc(unsigned char * data, int datalen)
--- libUtility/Utility.h
+++ libUtility/Utility.h
@@ -1,23 +1,90 @@
 #pragma once
+#include "stdafx.h"
 #include <stdio.h> 
 #include <cstdarg>
 #include <string> 
 #include <tchar.h>
 //#include <atlstr.h> //预编译头错误
 #include <list>
+#include <iostream>
+
+
 
 struct sParam
 {
 	unsigned char * ParamData;
 	int ParamLen;
 };
+typedef list<sParam> PARAMLIST;
+
 class Utility
 {
 public:
 
+	//************************************
+	// Method:    装包
+	// FullName:  Utility::toPackData
+	// Access:    public static 
+	// Returns:   unsigned char *
+	// Qualifier:
+	// Parameter: unsigned char * cmd
+	// Parameter: unsigned char fun
+	// Parameter: int count
+	// Parameter: ...
+	//************************************
 	static unsigned char * toPackData(unsigned char *cmd, unsigned char fun, int count, ...);
+	//************************************
+	// Method:    结包
+	// FullName:  Utility::unPackData
+	// Access:    public static 
+	// Returns:   int
+	// Qualifier:
+	// Parameter: unsigned char * buffer
+	// Parameter: int mun
+	// Parameter: PARAMLIST params
+	//************************************
+	static int unPackData(unsigned char * buffer, int mun, PARAMLIST *params);
+	//************************************
+	// Method:    Int2Byte[]
+	// FullName:  Utility::intToByteArray
+	// Access:    public static 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: int a
+	// Parameter: unsigned char * b
+	//************************************
 	static void intToByteArray(int a, unsigned char *b);
+
+
+	//************************************
+	// Method:    Byte[] 2 Int
+	// FullName:  Utility::ByteArrayToInt
+	// Access:    public static 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: unsigned char * b
+	// Parameter: int a
+	//************************************
+	static int ByteArrayToInt(unsigned char *b);
+	//************************************
+	// Method:    cr_bcc
+	// FullName:  Utility::cr_bcc
+	// Access:    public static 
+	// Returns:   unsigned char
+	// Qualifier:
+	// Parameter: unsigned char * data
+	// Parameter: int datalen
+	//************************************
 	static unsigned char cr_bcc(unsigned char * data, int datalen);
+	//************************************
+	// Method:    GB18030ToUTF8
+	// FullName:  Utility::GB18030ToUTF8
+	// Access:    public static 
+	// Returns:   int
+	// Qualifier:
+	// Parameter: char * pStrGB18030
+	// Parameter: unsigned char * * pStrUtf8
+	//************************************
 	static int GB18030ToUTF8(char *pStrGB18030, unsigned char **pStrUtf8);
 	Utility();
 	~Utility();
