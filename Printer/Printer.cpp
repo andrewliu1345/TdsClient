5576435fd0049949d1e3e5a7a65daf7cf23b6262
andrewliu1345 <andrewldq@qq.com>
2018-06-25 18:57:25 +0800

    修改打印超时

--- Printer/Printer.cpp
+++ Printer/Printer.cpp
@@ -219,7 +219,7 @@ int Printer::printForm(const char * formName, const char * content, int * pReqID
 	InterlockedIncrement((LPLONG)&iReqid);
 	if (iRet > 0)
 	{
-		iRet = transoket->ReadData(this, 6000);//开启读取异步线程
+		iRet = transoket->ReadData(this, 2000);//开启读取异步线程
 		return DEVICE_ERROR_SUCCESS;
 	}
 	else
